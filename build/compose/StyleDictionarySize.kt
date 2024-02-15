9de20e71316860d947b1a8de32638add73d6b433
robertmaugerglobant <robertmaugerglobant@users.noreply.github.com>
2024-02-15 03:57:49 +0000

    Apply automatic changes

--- build/compose/StyleDictionarySize.kt
+++ build/compose/StyleDictionarySize.kt
@@ -1,7 +1,7 @@
 
 
 // Do not edit directly
-// Generated on Thu, 15 Feb 2024 03:53:57 GMT
+// Generated on Thu, 15 Feb 2024 03:57:48 GMT
 
 
 
--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Feb 2024 03:53:57 GMT
+// Generated on Thu, 15 Feb 2024 03:57:48 GMT
 
 
 #import "StyleDictionarySize.h"
