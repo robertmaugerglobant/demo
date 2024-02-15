144a3ab30baba19436c304e6f9db1f63de8df5c4
robertmaugerglobant <robertmaugerglobant@users.noreply.github.com>
2024-02-15 01:29:48 +0000

    Apply automatic changes

---
+++ build/compose/StyleDictionaryColor.kt
@@ -0,0 +1,78 @@
+
+
+// Do not edit directly
+// Generated on Thu, 15 Feb 2024 01:29:47 GMT
+
+
+
+package StyleDictionaryColor;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionaryColor {
+  val colorActionAccent = #FFFEFA
+  val colorActionActive = #1A31A9
+  val colorActionBase = #191919
+  val colorActionDisabled = #ABABAB
+  val colorActionFocus = #3F59E4
+  val colorActionFocusRing = #8CA4F5
+  val colorActionHover = #3F59E4
+  val colorActionLinkDisabled = #808080
+  val colorBaseGrayDark = Color(0xff111111)
+  val colorBaseGrayLight = Color(0xffcccccc)
+  val colorBaseGrayMedium = Color(0xff999999)
+  val colorBaseGreen = Color(0xff00ff00)
+  val colorBaseRed = Color(0xffff0000)
+  val colorBorderPrimary = #BFBAAC
+  val colorBoxShadowBase = #191919
+  val colorBoxShadowShade = #191919
+  val colorBrandPrimaryBlueCloud = #B6C9FF
+  val colorBrandPrimaryBlueCobalt = #1A31A9
+  val colorBrandPrimaryBlueSky = #3F59E4
+  val colorBrandPrimaryNeutralCarbon = #191919
+  val colorBrandPrimaryNeutralCream = #F6F1E7
+  val colorBrandPrimaryNeutralGravel = #AFABA1
+  val colorBrandPrimaryNeutralSand = #E8DCC7
+  val colorBrandPrimaryNeutralSlate = #6B665F
+  val colorBrandPrimaryNeutralSnow = #FFFEFA
+  val colorBrandPrimaryTealOcean = #096256
+  val colorBrandPrimaryTealSeafoam = #4CB7A3
+  val colorBrandPrimaryTealTurquoise = #B1E4DE
+  val colorBrandSecondaryAmberGoldenrod = #EF9B05
+  val colorBrandSecondaryAmberWalnut = #744E00
+  val colorBrandSecondaryAmberWheat = #F4D594
+  val colorBrandSecondaryGreenEmerald = #149750
+  val colorBrandSecondaryGreenForest = #0F5C34
+  val colorBrandSecondaryGreenPistachio = #8FC88A
+  val colorBrandSecondaryOrangeClay = #763101
+  val colorBrandSecondaryOrangeMelon = #F2AC84
+  val colorBrandSecondaryOrangeTangerine = #E27133
+  val colorBrandSecondaryPurpleEggplant = #4016A1
+  val colorBrandSecondaryPurpleLilac = #B59DFC
+  val colorBrandSecondaryPurpleViolet = #7649F2
+  val colorContentPrimary = #191919
+  val colorContentSecondary = #4F4C4A
+  val colorFontBase = Color(0xffff0000)
+  val colorFontSecondary = Color(0xff00ff00)
+  val colorFontTertiary = Color(0xffcccccc)
+  val colorFormAccent = #ABABAB
+  val colorFormBackground = #FFFEFA
+  val colorFormBase = #191919
+  val colorFormDisabled = #E5E5E5
+  val colorFormError = #B2551E
+  val colorFormErrorFocusRing = #F8C1A3
+  val colorFormFocus = #3F59E4
+  val colorFormFocusRing = #A0B6FA
+  val colorFormHover = #E5E5E5
+  val colorGradientBlue = linear-gradient(135deg, #F6F1E7 0%, #3F59E4 60%, #1A31A9 90%)
+  val colorGradientCarbon = linear-gradient(135deg, #F6F1E7 0%, #6B665F 60%, #191919 90%)
+  val colorGradientCream = linear-gradient(135deg, #FFFEFA 0%, #F6F1E7 60%, #E8DCC7 90%)
+  val colorGradientDevx = linear-gradient(135deg, #191919 5%, #4016A1 30%, #3F59E4 50%, #F2AC84 70%, #F6F1E7 95%)
+  val colorGradientTeal = linear-gradient(135deg, #F6F1E7 0%, #4CB7A3 60%, #096256 90%)
+  val colorSurfaceBase = #FFFEFA
+  val colorSurfaceDim = #E8DCC7
+  val colorSurfaceGradient = linear-gradient(135deg, #FFFEFA 0%, #F6F1E7 60%, #E8DCC7 90%)
+  val colorSurfaceLight = #FFFFFF
+  val colorSurfaceMid = #F6F1E7
+}
---
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -0,0 +1,77 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 15 Feb 2024 01:29:47 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryColor {
+    public static let actionAccent = #FFFEFA
+    public static let actionActive = #1A31A9
+    public static let actionBase = #191919
+    public static let actionDisabled = #ABABAB
+    public static let actionFocus = #3F59E4
+    public static let actionFocusRing = #8CA4F5
+    public static let actionHover = #3F59E4
+    public static let actionLinkDisabled = #808080
+    public static let baseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    public static let baseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let baseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let baseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let baseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let borderPrimary = #BFBAAC
+    public static let boxShadowBase = #191919
+    public static let boxShadowShade = #191919
+    public static let brandPrimaryBlueCloud = #B6C9FF
+    public static let brandPrimaryBlueCobalt = #1A31A9
+    public static let brandPrimaryBlueSky = #3F59E4
+    public static let brandPrimaryNeutralCarbon = #191919
+    public static let brandPrimaryNeutralCream = #F6F1E7
+    public static let brandPrimaryNeutralGravel = #AFABA1
+    public static let brandPrimaryNeutralSand = #E8DCC7
+    public static let brandPrimaryNeutralSlate = #6B665F
+    public static let brandPrimaryNeutralSnow = #FFFEFA
+    public static let brandPrimaryTealOcean = #096256
+    public static let brandPrimaryTealSeafoam = #4CB7A3
+    public static let brandPrimaryTealTurquoise = #B1E4DE
+    public static let brandSecondaryAmberGoldenrod = #EF9B05
+    public static let brandSecondaryAmberWalnut = #744E00
+    public static let brandSecondaryAmberWheat = #F4D594
+    public static let brandSecondaryGreenEmerald = #149750
+    public static let brandSecondaryGreenForest = #0F5C34
+    public static let brandSecondaryGreenPistachio = #8FC88A
+    public static let brandSecondaryOrangeClay = #763101
+    public static let brandSecondaryOrangeMelon = #F2AC84
+    public static let brandSecondaryOrangeTangerine = #E27133
+    public static let brandSecondaryPurpleEggplant = #4016A1
+    public static let brandSecondaryPurpleLilac = #B59DFC
+    public static let brandSecondaryPurpleViolet = #7649F2
+    public static let contentPrimary = #191919
+    public static let contentSecondary = #4F4C4A
+    public static let fontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let fontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    public static let fontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let formAccent = #ABABAB
+    public static let formBackground = #FFFEFA
+    public static let formBase = #191919
+    public static let formDisabled = #E5E5E5
+    public static let formError = #B2551E
+    public static let formErrorFocusRing = #F8C1A3
+    public static let formFocus = #3F59E4
+    public static let formFocusRing = #A0B6FA
+    public static let formHover = #E5E5E5
+    public static let gradientBlue = linear-gradient(135deg, #F6F1E7 0%, #3F59E4 60%, #1A31A9 90%)
+    public static let gradientCarbon = linear-gradient(135deg, #F6F1E7 0%, #6B665F 60%, #191919 90%)
+    public static let gradientCream = linear-gradient(135deg, #FFFEFA 0%, #F6F1E7 60%, #E8DCC7 90%)
+    public static let gradientDevx = linear-gradient(135deg, #191919 5%, #4016A1 30%, #3F59E4 50%, #F2AC84 70%, #F6F1E7 95%)
+    public static let gradientTeal = linear-gradient(135deg, #F6F1E7 0%, #4CB7A3 60%, #096256 90%)
+    public static let surfaceBase = #FFFEFA
+    public static let surfaceDim = #E8DCC7
+    public static let surfaceGradient = linear-gradient(135deg, #FFFEFA 0%, #F6F1E7 60%, #E8DCC7 90%)
+    public static let surfaceLight = #FFFFFF
+    public static let surfaceMid = #F6F1E7
+}
